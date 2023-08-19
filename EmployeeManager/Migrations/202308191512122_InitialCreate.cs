52002ca931eea18394a7e44e9b7cbd558f9c8c57
tomaslm3 <tomaslm_3@hotmail.com>
2023-08-19 12:13:43 -0300

    Fix using migrations

---
+++ EmployeeManager/Migrations/202308191512122_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿namespace EmployeeManager.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.EmployeeModels",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Firstname = c.String(nullable: false),
+                        Lastname = c.Int(nullable: false),
+                        Email = c.Int(nullable: false),
+                        Salary = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.EmployeeModels");
+        }
+    }
+}
--- EmployeeManager/Models/EmployeeContext.cs
+++ EmployeeManager/Models/EmployeeContext.cs
@@ -2,9 +2,6 @@
 
 namespace EmployeeManager.Models {
     public class EmployeeContext : DbContext {
-        public EmployeeContext() : base("name=EmployeeContext") {
-            Database.SetInitializer(new CreateDatabaseIfNotExists<EmployeeContext>());
-        }
 
         public DbSet<EmployeeModel> Employees { get; set; }
     }
