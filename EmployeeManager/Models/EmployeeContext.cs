460beeaec5ac671bc65a74d271ef1e0c390ff4ab
tomaslm3 <tomaslm_3@hotmail.com>
2023-08-19 12:08:35 -0300

    Added Employee context

---
+++ EmployeeManager/Models/EmployeeContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace EmployeeManager.Models {
+    public class EmployeeContext : DbContext {
+        public EmployeeContext() : base("name=EmployeeContext") {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<EmployeeContext>());
+        }
+
+        public DbSet<EmployeeModel> Employees { get; set; }
+    }
+}
