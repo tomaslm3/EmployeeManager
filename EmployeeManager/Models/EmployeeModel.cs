33e5d00c0afee4b9d54e2679c3c633c9a41e8a9b
tomaslm3 <tomaslm_3@hotmail.com>
2023-08-19 12:05:37 -0300

    Adde EmployeeModel

---
+++ EmployeeManager/Models/EmployeeModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class EmployeeModel {
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Firstname { get; set; }
+
+    [Required]
+    public int Lastname { get; set; }
+
+    [Required]
+    public int Email { get; set; }
+
+    [Required]
+    public string Salary { get; set; }
+}
