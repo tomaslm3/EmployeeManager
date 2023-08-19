3d26af856816fcbbf8731fa50cba34e8790c0876
tomaslm3 <tomaslm_3@hotmail.com>
2023-08-19 10:53:06 -0300

    Agregar archivos de proyecto.

---
+++ EmployeeManager.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace EmployeeManager.Tests {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
